c8c37d5c73819c6cad8558d0b8394f26f5e5ea18
Vladimir Kuznetsov <mistakster@gmail.com>
2023-07-31 08:29:56 +0400

    updated tests for the tokenizer

--- test/tokenize.spec.ts
+++ test/tokenize.spec.ts
@@ -3,43 +3,82 @@ import fc from 'fast-check';
 import { tokenize } from "../lib/tokenize";
 
 describe("tokenize", () => {
-  it("should tokenize empty strings and return an empty array", () => {
+  it("should tokenize empty string and return an empty array", () => {
     fc.assert(
-      fc.property(fc.constant(""), (input: string) => {
-        const result = tokenize(input);
-        deepStrictEqual(result, []);
-      })
+      fc.property(
+        fc.constant(""),
+        (input) => {
+          const result = tokenize(input);
+          deepStrictEqual(result, []);
+        }
+      )
     );
   });
 
   it("should tokenize strings with only special characters and return an empty array", () => {
-    const args = fc.stringOf(fc.stringMatching(/^\W+$/), { minLength: 1 });
     fc.assert(
-      fc.property(args, (input: string) => {
-        const result = tokenize(input);
-        deepStrictEqual(result, []);
-      })
+      fc.property(
+        fc.stringOf(fc.stringMatching(/^\W+$/), { minLength: 1 }),
+        (input) => {
+          const result = tokenize(input);
+          deepStrictEqual(result, []);
+        }
+      )
     );
   });
 
-  it("should tokenize strings with only numbers and return an empty array", () => {
-    const args = fc.stringOf(fc.mixedCase(fc.hexa()), { minLength: 1 });
+  it("should tokenize strings with alpha-numeric characters", () => {
     fc.assert(
-      fc.property(args, (input: string) => {
-        const result = tokenize(input);
-        deepStrictEqual(result, [input.toLowerCase()]);
-      })
+      fc.property(
+        fc.stringOf(fc.stringMatching(/^\w+$/), { minLength: 1 }),
+        (input) => {
+          const result = tokenize(input);
+          deepStrictEqual(result, [input.toLowerCase()]);
+        }
+      )
     );
   });
 
   it("should tokenize strings with multiple spaces", () => {
-    const args = fc.stringOf(fc.stringMatching(/^\w+$/), { minLength: 1 });
     fc.assert(
-      fc.property(args, (input: string) => {
-        const inputWithSpaces = `   ${input}   `;
-        const result = tokenize(inputWithSpaces);
-        deepStrictEqual(result, [input.toLowerCase()]);
-      })
+      fc.property(
+        fc.stringOf(fc.stringMatching(/^\w+$/), { minLength: 1 }),
+        (input) => {
+          const inputWithSpaces = `   ${input}   `;
+          const result = tokenize(inputWithSpaces);
+          deepStrictEqual(result, [input.toLowerCase()]);
+        }
+      )
     );
+  });
+
+  it("should ignore quotes while tokenizing", () => {
+    fc.assert(
+      fc.property(
+        fc.record({
+          tuple: fc.tuple(
+            fc.stringOf(fc.mixedCase(fc.hexa()), { minLength: 1 }),
+            fc.stringOf(fc.mixedCase(fc.hexa()), { minLength: 1 })
+          ),
+          delimiter: fc.stringOf(fc.constantFrom('"', '\''), { minLength: 1, maxLength: 1 })
+        }),
+        (input) => {
+          const result = tokenize(input.tuple.join(input.delimiter));
+          deepStrictEqual(result, [input.tuple.join('').toLowerCase()]);
+        }
+      )
+    );
+  });
+
+  it("should tokenize text", () => {
+    fc.assert(
+      fc.property(
+        fc.lorem(),
+        (input) => {
+          const result = tokenize(input);
+          deepStrictEqual(result, input.split(/\s+/));
+        }
+      )
+    )
   });
 });
